5d8769b41dd1b4eced04b1920acd358a7fc4488b
gordon <gordon.song@thoughtworks.com>
2018-12-04 12:33:43 +0800

    Added support for spare frame

--- src/bowling-game.js
+++ src/bowling-game.js
@@ -4,18 +4,33 @@ class Game {
   }
 
   roll(pin) {
-    this.scoreArray.push(pin)
+    this.scoreArray.push(pin);
   }
 
   score() {
-    let score = 0;
+    let totalScore = 0;
     let rollIndex = 0;
 
     for (let frame = 1; frame <= 10; frame++) {
-      score += this.scoreArray[rollIndex] + this.scoreArray[rollIndex + 1];
+      const firstThrow = this.scoreArray[rollIndex];
+      const secondThrow = this.scoreArray[rollIndex + 1];
+      const firstThrowOfNextFrame = this.scoreArray[rollIndex + 2];
+
+      if (this.isSpareFrame(firstThrow, secondThrow)) {
+        totalScore += this.spareFrameScore(firstThrow, secondThrow, firstThrowOfNextFrame);
+      } else {
+        totalScore += firstThrow + secondThrow;
+      }
+
       rollIndex += 2;
     }
-    return score;
+    return totalScore;
+  }
+  isSpareFrame(firstThrow, secondThrow) {
+    return firstThrow + secondThrow === 10;
+  }
+  spareFrameScore(firstThrow, secondThrow, firstThrowOfNextFrame) {
+    return firstThrow + secondThrow + firstThrowOfNextFrame;
   }
 }
 
--- src/bowling-game.test.js
+++ src/bowling-game.test.js
@@ -1,18 +1,34 @@
 const Game = require("./bowling-game");
+
+let game;
+beforeEach(() => {
+  game = new Game();
+});
+
 test("test we can handle a gutter game session", () => {
-  const game = new Game();
-  rollMany(game,20,0)
+  rollMany(game, 20, 0);
   expect(game.score()).toEqual(0);
 });
 
 test("test a game with 20 rolls with a score of 1", () => {
-  const game = new Game()
-  rollMany(game,20,1)
-  expect(game.score()).toEqual(20)
-})
+  rollMany(game, 20, 1);
+  expect(game.score()).toEqual(20);
+});
 
-function rollMany(game,n,pins) {
-  for (let i = 0; i<n; i++) {
-    game.roll(pins)
+test("should return score of 16 with first frame as a spare", () => {
+  rollSpare(game);
+  game.roll(3);
+  rollMany(game, 17, 0);
+  expect(game.score()).toEqual(16);
+});
+
+function rollSpare(game) {
+  game.roll(5);
+  game.roll(5);
+}
+
+function rollMany(game, n, pins) {
+  for (let i = 0; i < n; i++) {
+    game.roll(pins);
   }
 }
